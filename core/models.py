76b801423b57dc3eadff25c840c0bdef2cd6f613
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-22 16:51:40 +0300

    add client and service column to the db

--- core/models.py
+++ core/models.py
@@ -22,4 +22,20 @@ class Service(models.Model):
         return self.name
 
 #recent work model
+class RecentWork(models.Model):
+    title = models.CharField(max_length=100,verbose_name='work title')
+    image = models.ImageField(upload_to='works')
+
+    def __str__(self):
+        return self.title
+
+
+
 #client model
+class Client(models.Model):
+    name = models.CharField(max_lenth=100, verbose_name='client name')
+    description = models.TextField(verbose_name='client say')
+    image = models.ImageField(upload_to='clients', default='default.png')
+
+    def __str__(self):
+        return self.name
